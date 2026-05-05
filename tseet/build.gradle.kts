bffeeeffad8fc4f8a0467bf3102a4f28faa5f0a9
deref3046 <fine4trn@163.com>
2026-05-05 21:40:23 +0800

    [skip ci] clear branch

--- tseet/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-val verName by extra("v1.1.0")
-val verCode by extra(
-    providers.exec {
-        commandLine("git", "rev-list", "HEAD", "--count")
-    }.standardOutput.asText.get().trim().toInt() + 39
-)
